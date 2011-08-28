441294eba9f6c03f410dc2ee2e0063912de852f6
sodaclop <sodaclop+git@gmail.com>
2011-08-27 23:10:04 -0700

    cosmetic changes to header file

--- order_maintenance.h
+++ order_maintenance.h
@@ -15,16 +15,20 @@ bool ordmain_in_order(const struct ordmain_node * x, const struct ordmain_node *
 
 /* 
 Returns a pointer to a newly created node placed just after x. Nodes
-are members of only one list. if x is NULL, creates a list with a
+are members of only one list. If x is NULL, creates a list with a
 single node, then returns a pointer to that node.
 */
-struct ordmain_node * ordmain_insert_after(struct ordmain_node * x);
+struct ordmain_node * 
+ordmain_insert_after(struct ordmain_node * x);
 
+/*
+See ordmain_insert_after.
+ */
 struct ordmain_node * 
 ordmain_insert_before(struct ordmain_node *);
 
 /*
-Removes the node x from the list it belongs to and frees the memory it
+Removes the node x from the list it belongs to, then frees the memory it
 was using.
 */
 void ordmain_delete(struct ordmain_node * x); 
